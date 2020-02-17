6b2723ce1cd6629afd31b20b12520b1e61366d7e
haidt <dotruonghaiitk14@gmail.com>
2020-02-17 17:48:04 +0700

    firt commit

---
+++ ResturantAPI/Models/IDataRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResturantAPI.Models
+{
+    public interface IDataRepository<TEntity>
+    {
+        IEnumerable<TEntity> GetAll();
+        TEntity Get(int id);
+        void Add(TEntity entity);
+        void Change(TEntity dbEntity, TEntity entity);
+        void Delete(TEntity entity);
+
+    }
+}
